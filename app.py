6d70bce160248eec6ae79650685ac52391617ae9
Robert Tsui <rt.roberttsui@gmail.com>
2025-11-12 09:56:32 -0800

    Completed setting up the homepage that shows all lists

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>This is a todo starter!</h1>"
+def get_lists():
+    lists = [
+        {'title': 'Groceries', 'todos': []},
+        {'title': 'Groceries', 'todos': []},
+    ]
+    return render_template('lists.html', lists=lists)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
